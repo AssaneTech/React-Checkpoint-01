e367f58c82820b6f294daacd1ee399eda2eeb676
AssaneTech <assanediouf0063gmail.com>
2025-06-11 22:01:49 +0000

    Creating my react project product card first version

---
+++ src/product.js
@@ -0,0 +1,8 @@
+const product = {
+  name: "AssaneSMART",
+  price: "$299.99",
+  description: "A powerful smartphone that can do almost everything!Even CODING!",
+  image: "/images/AssaneSmart.jpg"
+};
+
+export default product;
